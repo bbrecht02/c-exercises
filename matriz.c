3dc9c9bf49db7865c9a717284b8f1ec6b59d73c5
bbrecht02 <bennyson.bgcorreia@gmail.com>
2019-08-20 18:22:27 -0300

    att

--- matriz.c
+++ matriz.c
@@ -23,17 +23,26 @@ int main() {
             scanf("%d", &matriz02[i][j]);
         }
     }
-
+    printf("matriz 01: ");
+    printf("\n");
     for (i = 0; i < 3; i++) {
+        printf("\n");
         for (j = 0; j < 3; j++) {
-            printf ("\n%d %d %d\n", i, j, matriz01[i][j]);
+            printf ("%d", matriz01[i][j]);
+            
         }
     }
+    printf("\n");
+    printf("\n");
+    printf("matriz 02: ");
+    printf("\n");
     for (i = 0; i < 3; i++) {
+        printf("\n");
         for(j = 0; j < 3; j++) {
-            printf ("\n%d %d %d\n", i, j, matriz02[i][j]);
+            printf ("%d", matriz02[i][j]);
         }
     }
+    printf("\n");
 
     return 0;
 }
