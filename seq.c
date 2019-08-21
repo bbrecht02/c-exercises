08794b97e82c7957308c8f8d8b48fcc9297706d8
bbrecht02 <bennyson.bgcorreia@gmail.com>
2019-08-21 21:44:11 +0000

    add

---
+++ seq.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int cont;
+    printf("digite uma sequencia de inteiros: ");
+    for (cont = 0; cont < 10; cont++) {
+        int seq = 0;
+        scanf("%d", &seq);
+        printf("\n%d", seq);
+    }
+
+
+
+
+}
