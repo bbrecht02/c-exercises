24047be262baf492b9b5d4b9947b8f6c9ef2f3fe
bbrecht02 <bennyson.bgcorreia@gmail.com>
2019-08-29 18:21:12 +0000

    add

---
+++ string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() { 
+
+        char a[100];
+	char b[100];
+	int i;
+
+	printf("Digite duas palavras para comparar: ");
+	printf("\n");
+
+
+	for (i = 0; i < 100; i++) {
+		
+	scanf("%s", &a);
+	scanf("%s", &b);
+	}
+
+
+		
+
+
+
+
+
+
+
+return 0; 
+}
