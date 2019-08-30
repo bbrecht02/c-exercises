7e7ed58744fd1ee5faee7576a958ae8d9965cdb3
Bennyson Brecht <49658184+bbrecht02@users.noreply.github.com>
2019-08-30 12:16:34 -0300

    Update bbbgc.c

--- bbbgc.c
+++ bbbgc.c
@@ -4,21 +4,38 @@ int main() {
 
 	//
 	int size;
-	int hash[size];
 	int number_cases;
 	int keys;
-	int address;
+	char address;
+	int i = 0 ;
 
 	scanf("%d", &number_cases);
 	scanf("%d", &keys);
 	scanf("%d", &size);
-	for (int i = 0; i < size; i++) { 
-		scanf("%d", hash[i]);
-	}	
+
+	char hash[size];
+
+	while (i < size -1 ) {
+		scanf("%s", &address);
+		i++;
+	}
+	hash[size] = address;
+	for (int c = 0; c < size; c++) {
+		printf("%d", hash[i]);
+	}
 
 
 
 
 
 
+
+
+		
+
+
+
+
+
+return 0;
 }
