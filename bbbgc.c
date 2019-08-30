e137e48242e02fc0ea6c433bc9e5fe36666d7c41
bbrecht02 <bennyson.bgcorreia@gmail.com>
2019-08-30 00:47:19 -0300

    hash table

---
+++ bbbgc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() { 
+
+	//
+	int size;
+	int hash[size];
+	int number_cases;
+	int keys;
+	int address;
+
+	scanf("%d", &number_cases);
+	scanf("%d", &keys);
+	scanf("%d", &size);
+	for (int i = 0; i < size; i++) { 
+		scanf("%d", hash[i]);
+	}	
+
+
+
+
+
+
+}
