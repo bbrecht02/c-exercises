e04d070d045c350751ee70e0d6ece5a9f10cd827
bbrecht02 <bennyson.bgcorreia@gmail.com>
2019-09-05 23:40:44 -0300

    a

--- stack.c
+++ stack.c
@@ -1,40 +1,57 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 int main(void)
-{
-    int stack[10];
+{    
+
+    int i = 0 , len = 0;
+    int stack[i];
     int *pointer;
     char usr;
-    int i = 0;
-
-    pointer = &stack[0];
-
-    
+    //int ultimo = len - 1;
+    int num;
 
 
-    puts("o que deseja fazer? / empilhar / desempilhar / imprimir /  ");
+    pointer = &stack[i];
+
+
+    puts("qual operação deseja realizar? / empilhar / desempilhar / imprimir /  ");
+    scanf("%s", &usr);
+
     
-    
-    
-    if (usr = 'desempilhar')
+    if (strcmp(usr, 'empilhar') == 0) 
     {
-        puts('desempilhar');
-    }    
-    
-    
-    
-    while (i == 0)
+
+        puts("qual numero deseja adicionar? \n");
+        scanf("%i", &num);
+        *pointer = stack[i++];
+        malloc(sizeof(stack[i]));
+        stack[i-1] = num;
+        len += 1;
+        
+    }
+    if (strcmp(usr, 'desempilhar') == 0) 
     {
-        scanf("%s", &usr);
+        *pointer = stack[--i];
+        len -= 1;
+    }
+    if (strcmp(usr, 'imprimir') == 0) 
+    {
+        printf("%i\n", *pointer);
     }
 
-
     
 
-
-
+    
+    
+    
+  
+    
+    
+    
+   
 
 
 
